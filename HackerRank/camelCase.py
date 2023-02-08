c670cb39dbe5e2c55817e42e1fae57fb579a6a74
Youssef Khafaga <82235614+YoussefKhafaga@users.noreply.github.com>
2023-02-08 20:25:27 +0200

    Add files via upload

---
+++ HackerRank/camelCase.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'camelcase' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def camelcase(s):
+    # Write your code here
+    res = 1
+    for char in s:
+        if char.isupper():
+            res += 1
+    return res
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = camelcase(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
