44c02a2c4064b8a158d015e2812eef9e2b05682a
Youssef Khafaga <82235614+YoussefKhafaga@users.noreply.github.com>
2023-03-04 14:44:10 +0200

    Add files via upload

---
+++ LeetCode/70. Climbing Stairs.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        one = 1
+        two = 1
+        for i in range(n-2, -1, -1):
+            temp = one
+            one = one + two
+            two = temp
+        return one
+        
+
+        
