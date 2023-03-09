d2428469d318501426773c02e10a0ef08ec0cbdc
Youssef Khafaga <82235614+YoussefKhafaga@users.noreply.github.com>
2023-03-09 14:22:56 +0200

    Add files via upload

---
+++ LeetCode/217. Contains Duplicate.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public bool ContainsDuplicate(int[] nums) {
+     HashSet<int> set = new HashSet<int>();
+     for(int i = 0; i < nums.Length; i++){
+         if(set.Contains(nums[i]))
+            return true;
+         set.Add(nums[i]);
+     }
+     return false;
+    }
+}
